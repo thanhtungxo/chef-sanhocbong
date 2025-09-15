978a283ddf0a20bbea4af29076d629e292130fd1
Thanh Tung <tungthanhtran1909@gmail.com>
2025-09-15 18:00:57 +0700

    Sua loi chi man hinh ho ten

--- src/components/organisms/PersonalInfoStep.tsx
+++ src/components/organisms/PersonalInfoStep.tsx
@@ -1,17 +1,22 @@
+import React from 'react';
 import { FormInput } from '../atoms/FormInput';
 import { SectionTitle } from '../atoms/SectionTitle';
+import { StepNavigation } from '../molecules/StepNavigation';
 
 interface Props {
   fullName: string;
   setFullName: (val: string) => void;
   age: string;
   setAge: (val: string) => void;
+  onNext: () => void;
 }
 
-export const PersonalInfoStep: React.FC<Props> = ({ fullName, setFullName, age, setAge }) => (
+export const PersonalInfoStep: React.FC<Props> = ({ fullName, setFullName, age, setAge, onNext }) => (
   <div>
     <SectionTitle>Thông tin cá nhân</SectionTitle>
     <FormInput label="Họ và tên" value={fullName} onChange={setFullName} />
     <FormInput label="Tuổi" value={age} onChange={setAge} type="number" />
+    <StepNavigation onBack={() => {}} onNext={onNext} showBack={false} />
   </div>
 );
+
